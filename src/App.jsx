5e983f055e5e46741ddf78abddb3030990e8a58b
javahir1223 <javah1223@gmail.com>
2024-10-27 13:19:53 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  )
+}
+
+export default App
